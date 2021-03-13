d803ff85506b4904cbbf9d9b9237baeeeb827493
e <1372431588@qq.com>
2021-03-13 10:53:03 +0800

    搭建框架

---
+++ host/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+urlpatterns = [
+    path('/', index, name='index'),
+    path('/user/', user, name='user'),
+    path('/cpu/', cpu, name='cpu'),
+    path('/memory/', memory, name='memory'),
+    path('/disk/', disk, name='disk'),
+    path('/network/', network, name='network'),
+    path('/process/', process, name='process'),
+]
--- host/views.py
+++ host/views.py
@@ -1,3 +1,24 @@
+#host/views.py
 from django.shortcuts import render
-
 # Create your views here.
+def index(request):
+    pass
+    return  render(request, 'host/index.html', locals())
+def user(request):
+    pass
+    return  render(request, 'host/user.html', locals())
+def cpu(request):
+    pass
+    return  render(request, 'host/cpu.html', locals())
+def memory(request):
+    pass
+    return  render(request, 'host/memory.html', locals())
+def disk(request):
+    pass
+    return  render(request, 'host/disk.html', locals())
+def network(request):
+    pass
+    return  render(request, 'host/network.html', locals())
+def process(request):
+    pass
+    return  render(request, 'host/process.html', locals())
